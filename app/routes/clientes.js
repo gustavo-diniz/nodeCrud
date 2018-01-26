4b5af6c45efd17a5b7da20c6e0a7247e378cb0da
gustavo diniz <gdinsantos1@gmail.com>
2018-01-26 06:55:30 -0200

    connection factory

--- app/routes/clientes.js
+++ app/routes/clientes.js
@@ -3,5 +3,22 @@ module. exports = function(app) {
         res.json({ message: 'Ola node!',
                    version: '1.0.0'}); 
     });
+    
+    app.get("/produtos", function(req, res) {
+        console.log('prod1')
+        var mysql = require('mysql');
+        var connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'gds1903',
+            databese: 'Veiculos'
+        });
+        
+        connection.query('SELECT * FROM Veiculos.placa_a limit 10', function(err, results){
+            res.send(results);
+        });
+        
+        connection.end();
+    });
 }
             
